d2f2e8e2f3c25bacc7e9962beefa7b23b8c5c5e0
Alfix <168222788+Alfix-Januarivinter@users.noreply.github.com>
2025-01-17 14:25:11 +0100

    Add files via upload
    Updated cookie enter.py

--- cookie enter.py
+++ cookie enter.py
@@ -1,7 +1,7 @@
-#version 1.0.1 (release) if bug contact a developer https://github.com/Alfix-Januarivinter/cookie-enter.py
+#version 1.0.2 (release) if bug contact a developer https://github.com/Alfix-Januarivinter/cookie-enter.p
 exit = False
 cookies = 0
-cookie_multipluier = 1
+cookie_multi = 1
 print("welcome to cookie enter")
 while exit == False:
     exit = False
@@ -9,7 +9,6 @@ while exit == False:
     exit2 = False
     what_todo = ""
     print(cookies)
-    cookiep = 1 * cookie_multipluier
     what_todo = input("Menu: ")
     if what_todo == "enter":
         while exit1 == False:
@@ -18,55 +17,55 @@ while exit == False:
             if enter == "exit":
                 exit1 = True
             else:
-                cookies = cookies + cookiep
+                cookies = cookies + cookie_multi
     elif what_todo == "upgrade":
         while exit2 == False:
             print("cookies = " + str(cookies))
-            print("cookie multipluier = " + str(cookie_multipluier))
+            print("cookie multiplier = " + str(cookie_multi))
             print("*+1 = 100 *+2 = 200 *+5 = 450 *+10 = 800 *+20 = 1400 *+50 = 3000 *+100 = 4500")
             up = input("Upgrade: ")
             if up == "1":
                 if cookies >= 100:
                     cookies = cookies - 100
-                    cookie_multipluier = cookie_multipluier + 1
+                    cookie_multi = cookie_multi + 1
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "2":
                 if cookies >= 200:
                     cookies = cookies - 200
-                    cookie_multipluier = cookie_multipluier + 2
+                    cookie_multi = cookie_multi + 2
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "5":
                 if cookies >= 450:
                     cookies = cookies - 450
-                    cookie_multipluier = cookie_multipluier + 5
+                    cookie_multi = cookie_multi + 5
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "10":
                 if cookies >= 800:
                     cookies = cookies - 800
-                    cookie_multipluier = cookie_multipluier + 10
+                    cookie_multi = cookie_multi + 10
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "20":
                 if cookies >= 1400:
                     cookies = cookies - 1400
-                    cookie_multipluier = cookie_multipluier + 20
+                    cookie_multi = cookie_multi + 20
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "50":
                 if cookies >= 3000:
                     cookies = cookies - 3000
-                    cookie_multipluier = cookie_multipluier + 50
+                    cookie_multi = cookie_multi + 50
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "100":
                 if cookies >= 4500:
                     cookies = cookies - 4500
-                    cookie_multipluier = cookie_multipluier + 100
+                    cookie_multi = cookie_multi + 100
                 else:
-                    print("to litle cookies")
+                    print("to little cookies")
             elif up == "exit":
                 exit2 = True
             else:
