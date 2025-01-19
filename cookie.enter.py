12c7b64473b90068ac847cac3bbe48000eafaf76
Alfix <168222788+Alfix-Januarivinter@users.noreply.github.com>
2025-01-19 16:48:52 +0100

    Version 1.0.8
    Version 1.0.8 Changes: Optimized it! (Added values to equal the text and printing it instead of printing the text so that made it take up less space) Made some small changes. (Optimization Update!)

--- cookie.enter.py
+++ cookie.enter.py
@@ -1,6 +1,10 @@
-#version 1.0.7 (released) if bug contact a developer https://github.com/Alfix-Januarivinter/cookie-enter.py
+#version 1.0.8 (release) if bug contact a developer https://github.com/Alfix-Januarivinter/cookie-enter.py
 c = 0
 b = 1
+l = "To little cookies!"
+p = "Wrong input!"
+x = "exit"
+z = "esc"
 print("Welcome to Cookie enter!")
 while True:
     print(c)
@@ -8,8 +12,8 @@ while True:
     if a == "enter" or a == "e":
         while True:
             print(c)
-            e = input(": ")
-            if e == "exit" or e == "esc":
+            a = input(": ")
+            if a == x or a == z:
                 break
             else:
                 c = c + b
@@ -18,71 +22,71 @@ while True:
             print("cookies = " + str(c))
             print("cookie multiplier = " + str(b))
             print("+1 = 100 / +2 = 200 / +5 = 450 / +10 = 800 / +20 = 1400 / +50 = 3000 / +100 = 4500")
-            u = input("Upgrade: ")
-            if u == "exit" or u == "esc":
+            a = input("Upgrade: ")
+            if a == x or a == z:
                 break
             h = int(input("Upgrade times: "))
             if h < 0:
                 h = 0
-            if u == "1":
+            if a == "1":
                 h1 = h * 100
                 if c >= h1:
                     c = c - h1
                     b = b + h
                 else:
-                    print("to little cookies!")
-            elif u == "2":
+                    print(l)
+            elif a == "2":
                 h2 = h * 200
                 d2 = h * 2
                 if c >= h2:
                     c = c - h2
                     b = b + d2
                 else:
-                    print("to little cookies!")
-            elif u == "5":
+                    print(l)
+            elif a == "5":
                 h3 = h * 450
                 d3 = h * 5
                 if c >= h3:
                     c = c - h3
                     b = b + d3
                 else:
-                    print("to little cookies!")
-            elif u == "10":
+                    print(l)
+            elif a == "10":
                 h4 = h * 800
                 d4 = h * 10
                 if c >= h4:
                     c = c - h4
                     b = b + d4
                 else:
-                    print("to little cookies!")
-            elif u == "20":
+                    print(l)
+            elif a == "20":
                 h5 = h * 1400
                 d5 = h * 20
                 if c >= h5:
                     c = c - h5
                     b = b + d5
                 else:
-                    print("to little cookies!")
-            elif u == "50":
+                    print(l)
+            elif a == "50":
                 h6 = h * 3000
                 d6 = h * 50
                 if c >= h6:
                     c = c - h6
                     b = b + d6
                 else:
-                    print("to little cookies!")
-            elif u == "100":
+                    print(l)
+            elif a == "100":
                 h7 = h * 4500
                 d = h * 100
                 if c >= h7:
                     c = c - h7
                     b = b + d
                 else:
-                    print("to little cookies!")
+                    print(l)
             else:
-                print("wrong input!")
-    elif a == "exit" or a == "esc":
+                print(p)
+    elif a == x or a == z:
         break
     else:
-        print("wrong input!")
-print("bye!")
+        print(p)
+print("Bye!")
